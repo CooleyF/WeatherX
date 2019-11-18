ae35aa64f786398df6de979f09581e51b40cfa76
jecool17 <51241657+jecool17@users.noreply.github.com>
2019-11-18 18:58:45 -0500

    Initial commit

---
+++ DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace WeatherAppX
+{
+    public class DataService
+    {
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            dynamic data = null;
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+
+            }
+
+            return data;
+        }
+    }
+}
